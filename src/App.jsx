a201ed80d64781ffa332c067ea58e799ffb6eab5
premkumar199131 <premkumar.ashokan@talentship.io>
2025-01-27 18:03:54 +0530

    project setup

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+
+
+const App = () => {
+  return (
+    <div className='container'>
+    <nav className="navbar">
+      <h1>CRUD Operation Using ReactJs</h1>
+    </nav>
+
+    <table border={1}>
+      <thead>
+        <tr>
+          <th>User ID</th>
+          <th>User Name</th>
+          <th>User Email</th>
+          <th>User Address</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+       
+      </tbody>
+    </table>
+  </div>
+  )
+}
+
+export default App
